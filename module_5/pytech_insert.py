212cef72a7084713e8f7842cd8f6060a26b7096f
TheForgettables <107652782+TheForgettables@users.noreply.github.com>
2022-07-03 18:07:23 -0400

    Add files via upload

---
+++ module_5/pytech_insert.py
@@ -0,0 +1,22 @@
+import tkinter
+MongoDB: insert_one(thorin)
+thorin = {
+    "first_name":"thorin"
+    "last_name" : "oakenshield"
+    "student_id" : "1007"
+}
+thorin_student_id = students.insert_one(thorin).1007
+MongoDB: insert_one(bilbo)
+bilbo = {
+    "first_name" : "bilbo"
+    "last_name" : "baggins"
+    "student_id" : "1008"
+}
+thorin_student_id = students.insert_one(bilbo).1008
+MongoDB : insert_one(frodo)
+frodo = {
+    "first_name" : "frodo"
+    "last_name" : "baggins"
+    "student_id" : "1009"
+}
+frodo_student_id = students.insert_one(bilbo).1009
---
+++ module_5/pytech_queries.py
@@ -0,0 +1,7 @@
+MongoDB: find()
+docs = db.pytech.find({"student_id"})
+
+MongoDB: find_one(thorin)
+doc = db.pytech.find_one({"student_id": "1007"})
+doc = db.pytech.find_one({"student_id" : "1008"})
+doc = db.pytech.find_one({"student_id" : "1009"})
